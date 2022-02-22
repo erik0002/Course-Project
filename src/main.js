02c579541754e1ceecf6c5e9fbcaa7ece2d9ffee
Yeliazar <erik.ruzhakov@mail.ru>
2022-02-23 00:54:17 +0300

    add dialog modal window

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import { createApp } from 'vue';
 import App from './App';
 import BaseCard from '@/components/UI/BaseCard';
 import BaseButton from '@/components/UI/BaseButton';
+import BaseDialog from '@/components/UI/BaseDialog';
 
 
 
@@ -10,5 +11,6 @@ const app = createApp(App)
 
 app.component('base-card', BaseCard);
 app.component('base-button', BaseButton);
+app.component('base-dialog', BaseDialog);
 
 app.mount('#app');
